0256112f045ec2a617c1b159844271a7f5f4faa1
Igor Ferraz <igormanoelferraz2022@gmail.com>
2024-09-14 13:53:04 -0300

    update zoom font-size

--- src/components/form-livro.tsx
+++ src/components/form-livro.tsx
@@ -4,7 +4,7 @@ import { SelectList } from "./select-list";
 import { Button } from "./button";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { formSchemaLivro, FormValuesLivro } from "../schemas/livro";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { createLivro, deleteLivro, getLivrosById, updateLivro } from "../api/livro";
 
 
@@ -18,7 +18,9 @@ type props = {
     toast: any
 }
 
-export const FormLivro = ({ id, onClose, actionType, refresh,toast}: props) => {
+export const FormLivro = ({ id, onClose, actionType, refresh, toast}: props) => {
+
+    const [loading, setLoading] = useState(false);
 
     const {
         register,
@@ -30,8 +32,8 @@ export const FormLivro = ({ id, onClose, actionType, refresh,toast}: props) => {
     })
 
     const onSubmit: SubmitHandler<FormValuesLivro> = async (data) => {
-        console.log(data)
         try {
+
             if (actionType === 'create') {
                 await createLivro(data);
                 toast.success("Livro Adicionado!",{
@@ -94,96 +96,106 @@ export const FormLivro = ({ id, onClose, actionType, refresh,toast}: props) => {
 
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)} className="w-full p-2 flex flex-col gap-2">
-            <div className="flex gap-2">
-                <div className="flex-1">
-                    <Input
-                        label="Nome"
-                        type="text"
-                        register={register}
-                        name="nomeLivro"
-                        error={errors.nomeLivro?.message}
-                    />
-                </div>
-                <div className="w-2/6">
-                    <Input
-                        label="Edição"
-                        type="number"
-                        register={register}
-                        name="numeroEdicao"
-                        error={errors.numeroEdicao?.message}
-                    />
-                </div>
-            </div>
-
-            <div className="flex flex-col sm:flex-row gap-2">
-                <div className="flex-1">
-                    <Input
-                        label="Autor"
-                        type="text"
-                        register={register}
-                        name="autor"
-                        error={errors.autor?.message}
-                    />
-                </div>
-                <div className="flex-1">
-                    <Input
-                        label="Codigo de barras"
-                        type="text"
-                        register={register}
-                        name="codigoBarras"
-                        error={errors.codigoBarras?.message}
-                    />
-                </div>
-            </div>
-
-            <div className="flex flex-col sm:flex-row gap-2">
-                <div className="flex-1">
-                    <Input
-                        label="Data Lançamento"
-                        type="date"
-                        register={register}
-                        name="dataLancamento"
-                        error={errors.dataLancamento?.message}
-                    />
-                </div>
-
-                <div className="flex-1">
-                    <SelectList
-                        register={register}
-                        name="localLancamento"
-                        label="Local"
-                        options={['Curitiba']}
-                        error={errors.localLancamento?.message}
-                    />
-                </div>
-            </div>
-
-
-            <div className="flex gap-2">
-                {id ? (
-                    <>
-                        <Button
-                            onClick={handleDelete}
-                            text="Remover"
-                            type="button"
-                            color="bg-red-700"
+        <>
+        {
+            loading ?
+            <form onSubmit={handleSubmit(onSubmit)} className="w-full p-2 flex flex-col gap-2">
+                <div className="flex gap-2">
+                    <div className="flex-1">
+                        <Input
+                            label="Nome"
+                            type="text"
+                            register={register}
+                            name="nomeLivro"
+                            error={errors.nomeLivro?.message}
                         />
+                    </div>
+                    <div className="w-2/6">
+                        <Input
+                            label="Edição"
+                            type="number"
+                            register={register}
+                            name="numeroEdicao"
+                            error={errors.numeroEdicao?.message}
+                        />
+                    </div>
+                </div>
+
+                <div className="flex flex-col sm:flex-row gap-2">
+                    <div className="flex-1">
+                        <Input
+                            label="Autor"
+                            type="text"
+                            register={register}
+                            name="autor"
+                            error={errors.autor?.message}
+                        />
+                    </div>
+                    <div className="flex-1">
+                        <Input
+                            label="Codigo de barras"
+                            type="text"
+                            register={register}
+                            name="codigoBarras"
+                            error={errors.codigoBarras?.message}
+                        />
+                    </div>
+                </div>
+
+                <div className="flex flex-col sm:flex-row gap-2">
+                    <div className="flex-1">
+                        <Input
+                            label="Data Lançamento"
+                            type="date"
+                            register={register}
+                            name="dataLancamento"
+                            error={errors.dataLancamento?.message}
+                        />
+                    </div>
+
+                    <div className="flex-1">
+                        <SelectList
+                            register={register}
+                            name="localLancamento"
+                            label="Local"
+                            options={['Curitiba']}
+                            error={errors.localLancamento?.message}
+                        />
+                    </div>
+                </div>
+
+
+                <div className="flex gap-2">
+                    {id ? (
+                        <>
+                            <Button
+                                onClick={handleDelete}
+                                text="Remover"
+                                type="button"
+                                color="bg-red-700"
+                            />
+                            <Button
+                                text="Salvar"
+                                type="submit"
+                                color="bg-green-700"
+                            />
+                        </>
+                    ) : (
                         <Button
-                            text="Salvar"
+                            text="Adicionar"
                             type="submit"
                             color="bg-green-700"
                         />
-                    </>
-                ) : (
-                    <Button
-                        text="Adicionar"
-                        type="submit"
-                        color="bg-green-700"
-                    />
-                )}
+                    )}
+                </div>
+            </form>
+            : 
+            <div>
+                Carregando...
             </div>
-        </form>
+        }
+        
+        </>
 
     )
 }
